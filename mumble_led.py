4c5506fbd67f9211b385844225a62f3b60bf4d2b
murshies <>
2019-03-04 12:54:55 -0500

    Pass gpio_pin into tick to avoid hardcoded value

--- mumble_led.py
+++ mumble_led.py
@@ -13,7 +13,7 @@ def get_mumble_clients(port):
     return [sconn.raddr for sconn in psutil.net_connections()
             if sconn.laddr[1] == port and sconn.status == 'ESTABLISHED']
 
-def tick(led_is_on, prev_connect_count, mumble_port):
+def tick(led_is_on, prev_connect_count, mumble_port, gpio_pin):
     clients = get_mumble_clients(mumble_port)
     num_clients = len(clients)
     clients_connected = num_clients > 0
@@ -21,10 +21,10 @@ def tick(led_is_on, prev_connect_count, mumble_port):
     if clients_connected:
         led_is_on = not led_is_on
         led_gpio_state = GPIO.HIGH if led_is_on else GPIO.LOW
-        GPIO.output(18, led_gpio_state)
+        GPIO.output(gpio_pin, led_gpio_state)
     else:
         led_is_on = False
-        GPIO.output(18, GPIO.LOW)
+        GPIO.output(gpio_pin, GPIO.LOW)
 
     if num_clients != prev_connect_count:
         clients_join = ','.join('%s:%s' % client for client in clients)
@@ -51,7 +51,7 @@ def main():
 
     try:
         while True:
-            num_clients, led_is_on = tick(led_is_on, num_clients, mumble_port)
+            num_clients, led_is_on = tick(led_is_on, num_clients, mumble_port, gpio_pin)
             time.sleep(1)
     except:
         GPIO.output(gpio_pin, GPIO.LOW)
